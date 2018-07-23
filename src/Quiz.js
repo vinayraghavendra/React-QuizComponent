40e37c9869b4c86f63f6c9cdef33a455576e8a7a
vinay.raghavendrah.s@cognizant.com <476445@cognizant.com>
2018-07-23 11:40:21 +0530

    quizquestion spelling corrected

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,11 @@ class Quiz extends Component{
     }
     render(){
         return (
-            <div><div className="QuizQuestions">quizData.quiz_questions[0].instruction_text</div></div>
+            <div>
+                <div className="QuizQuestion">
+                    quizData.quiz_questions[0].instruction_text
+                </div>
+            </div>
         )
     }
 }
